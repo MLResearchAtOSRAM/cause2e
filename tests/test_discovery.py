f443aae63138e4dae46e431621330c87836f015a
dg46 <76687534+dg46@users.noreply.github.com>
2021-11-05 13:01:34 +0100

    Fix error in validation knowledge

--- tests/test_discovery.py
+++ tests/test_discovery.py
@@ -123,8 +123,9 @@ class TestFullAnalysis(unittest.TestCase):
         self.learner.binarize_variable('Season', one_val='Spring', zero_val='Winter')
         self.learner._create_estimator()
         # self.learner._estimator.run_quick_analysis('Season', 'Slippery', 'nonparametric-ate')
-        self.learner._estimator.run_multiple_quick_analyses(['Season', 'Wet'],
-                                                            ['Wet', 'Slippery'],
+        vars = list(self.learner.variables)
+        self.learner._estimator.run_multiple_quick_analyses(vars,
+                                                            vars,
                                                             ['nonparametric-ate', 'nonparametric-nde', 'nonparametric-nie'],
                                                             verbose=True,
                                                             show_tables=True,
