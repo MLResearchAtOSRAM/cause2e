caaeff4fa02c9b27b6d61e3f032e9d312a221c0c
dg46 <76687534+dg46@users.noreply.github.com>
2021-11-08 16:06:32 +0100

    Add tests for data generation

---
+++ tests/test_data_generator.py
@@ -0,0 +1,25 @@
+import unittest
+from cause2e.path_mgr import PathManager
+from cause2e.data_generator import DataGenerator
+import os
+
+
+class TestDataGenerator(unittest.TestCase):
+    def setUp(self):
+        pwd = os.getcwd()
+        data_dir = os.path.join(pwd, 'tests', 'fixtures', 'data')
+        paths = PathManager(experiment_name='foo',
+                            data_name='test_generation',
+                            data_dir=data_dir,
+                            output_dir='bar')
+        self.generator = DataGenerator(paths)
+
+    def test_generate_linear_dataset(self):
+        beta = 2
+        n_common_causes = 3
+        nrows = 1000
+        self.generator.generate_linear_dataset(beta, n_common_causes, nrows)
+        self.assertEqual(len(self.data), nrows)
+        self.assertEqual(len(self.data.columns), n_common_causes + 2)
+        self.generator.display_graph()
+        self.generator.write_csv()
